ca941244d923e306af2b934c59ae6511c8fc5145
peiyi <peiyi523@gmail.com>
2024-03-21 21:07:56 +0800

    完成代辦事項顯示功能

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from .models import Todo
 
 
 # Create your views here.
 def todo(request):
-    return render(request, "todo/todo.html")
+    # all,get,filter
+    # todos = Todo.objects.all()
+    # for todo in todos:
+    #     print(todo.title, todo.text)
+    
+    todos = None
+    if request.user.is_authenticated:
+        todos = Todo.objects.filter(user=request.user)
+
+    return render(request, "todo/todo.html", {"todos": todos})
